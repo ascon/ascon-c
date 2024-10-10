91724647988888fed80579d59d4ec200351e9c8f
Martin Schläffer <martin.schlaeffer@gmail.com>
2024-10-11 00:31:31 +0200

    Add ref to all builds of test-all.sh

--- scripts/test-all.sh
+++ scripts/test-all.sh
@@ -28,8 +28,8 @@ echo
 echo "Test clang $OPT builds:" | tee -a ../test-all.log | grep Test >&3
 rm -f CMakeCache.txt
 cmake .. -DCMAKE_C_COMPILER=clang -DALG_LIST="" -DIMPL_LIST=""
-cmake .. -UALG_LIST -UEMULATOR \
-         -DCMAKE_BUILD_TYPE=$TYPE \
+cmake .. -DCMAKE_BUILD_TYPE=$TYPE -UALG_LIST \
+         -UEMULATOR \
          -DREL_FLAGS="$OPT;-fomit-frame-pointer;-march=native;-mtune=native" \
          -DIMPL_LIST=$CLIST
 cmake --build . --clean-first -- -k | tee -a ../test-all.log | grep "Built target genkat" >&3
@@ -39,8 +39,8 @@ echo
 echo "Test gcc $OPT builds:" | tee -a ../test-all.log | grep Test >&3
 rm -f CMakeCache.txt
 cmake .. -DCMAKE_C_COMPILER=gcc -DALG_LIST="" -DIMPL_LIST=""
-cmake .. -UALG_LIST -UEMULATOR \
-         -DCMAKE_BUILD_TYPE=$TYPE \
+cmake .. -DCMAKE_BUILD_TYPE=$TYPE -UALG_LIST \
+         -UEMULATOR \
          -DREL_FLAGS="$OPT;-fomit-frame-pointer;-march=native;-mtune=native" \
          -DIMPL_LIST=$CLIST
 cmake --build . --clean-first -- -k | tee -a ../test-all.log | grep "Built target genkat" >&3
@@ -50,11 +50,11 @@ echo
 echo "Test avx512 $OPT builds:" | tee -a ../test-all.log | grep Test >&3
 rm -f CMakeCache.txt
 cmake .. -DCMAKE_C_COMPILER=gcc -DALG_LIST="" -DIMPL_LIST=""
-cmake .. -UALG_LIST -DEMULATOR="sde;--" \
-         -DCMAKE_BUILD_TYPE=$TYPE \
+cmake .. -DCMAKE_BUILD_TYPE=$TYPE -UALG_LIST \
+         -DEMULATOR="sde;--" \
          -DDBG_FLAGS="$OPT;-std=c99;-Wall;-Wextra;-Wshadow;-fsanitize=address,undefined;-march=icelake-client" \
          -DREL_FLAGS="$OPT;-fomit-frame-pointer;-march=icelake-client" \
-         -DIMPL_LIST="avx512"
+         -DIMPL_LIST="avx512;ref"
 cmake --build . --clean-first -- -k | tee -a ../test-all.log | grep "Built target genkat" >&3
 ctest | sed -u 's/[0-9.]* sec//g' | tee -a ../test-all.log | grep Test >&3
 
@@ -62,11 +62,11 @@ echo
 echo "Test neon $OPT builds:" | tee -a ../test-all.log | grep Test >&3
 rm -f CMakeCache.txt
 cmake .. -DCMAKE_C_COMPILER=arm-linux-gnueabi-gcc -DALG_LIST="" -DIMPL_LIST=""
-cmake .. -UALG_LIST -DEMULATOR="qemu-arm;-L;/usr/arm-linux-gnueabi" \
-         -DCMAKE_BUILD_TYPE=$TYPE \
+cmake .. -DCMAKE_BUILD_TYPE=$TYPE -UALG_LIST \
+         -DEMULATOR="qemu-arm;-L;/usr/arm-linux-gnueabi" \
          -DDBG_FLAGS="$OPT;-std=c99;-Wall;-Wextra;-Wshadow;-fsanitize=address,undefined;-latomic;-march=armv7-a" \
          -DREL_FLAGS="$OPT;-fomit-frame-pointer;-march=armv7-a" \
-         -DIMPL_LIST="neon"
+         -DIMPL_LIST="neon;ref"
 cmake --build . --clean-first -- -k | tee -a ../test-all.log | grep "Built target genkat" >&3
 ctest | sed -u 's/[0-9.]* sec//g' | tee -a ../test-all.log | grep Test >&3
 
@@ -74,8 +74,8 @@ echo
 echo "Test armv7m $OPT builds:" | tee -a ../test-all.log | grep Test >&3
 rm -f CMakeCache.txt
 cmake .. -DCMAKE_C_COMPILER=arm-linux-gnueabi-gcc -DALG_LIST="" -DIMPL_LIST=""
-cmake .. -UALG_LIST -DEMULATOR="qemu-arm;-L;/usr/arm-linux-gnueabi" \
-         -DCMAKE_BUILD_TYPE=$TYPE \
+cmake .. -DCMAKE_BUILD_TYPE=$TYPE -UALG_LIST \
+         -DEMULATOR="qemu-arm;-L;/usr/arm-linux-gnueabi" \
          -DREL_FLAGS="$OPT;-fomit-frame-pointer;-march=armv7" \
          -DDBG_FLAGS="$OPT;-std=c99;-Wall;-Wextra;-Wshadow;-fsanitize=address,undefined;-latomic;-march=armv7" \
          -DIMPL_LIST="$CLIST;armv6;armv6_lowsize;armv7m;armv7m_lowsize;armv7m_small;bi32_armv6;bi32_armv7m;bi32_armv7m_small;protected_bi32_armv6;protected_bi32_armv6_leveled"
@@ -86,11 +86,11 @@ echo
 echo "Test armv6m $OPT builds:" | tee -a ../test-all.log | grep Test >&3
 rm -f CMakeCache.txt
 cmake .. -DCMAKE_C_COMPILER=arm-none-eabi-gcc -DCMAKE_C_COMPILER_FORCED=ON -DALG_LIST="" -DIMPL_LIST=""
-cmake .. -UALG_LIST -DEMULATOR="qemu-system-arm;-semihosting;-nographic;-machine;microbit;-kernel" \
-         -DCMAKE_BUILD_TYPE=$TYPE \
+cmake .. -DCMAKE_BUILD_TYPE=$TYPE -UALG_LIST \
+         -DEMULATOR="qemu-system-arm;-semihosting;-nographic;-machine;microbit;-kernel" \
          -DREL_FLAGS="$OPT;-fomit-frame-pointer;--specs=picolibc.specs;--oslib=semihost;-T../tests/microbit.ld;-mcpu=cortex-m0" \
          -DDBG_FLAGS="$OPT;-std=c99;-Wall;-Wextra;-Wshadow;--specs=picolibc.specs;--oslib=semihost;-T../tests/microbit.ld;-mcpu=cortex-m0" \
-         -DIMPL_LIST="armv6m;armv6m_lowsize"
+         -DIMPL_LIST="armv6m;armv6m_lowsize;ref"
 cmake --build . --clean-first -- -k | tee -a ../test-all.log | grep "Built target genkat" >&3
 ctest | sed -u 's/[0-9.]* sec//g' | tee -a ../test-all.log | grep Test >&3
 
@@ -98,11 +98,11 @@ echo
 echo "Test rv32 $OPT builds:" | tee -a ../test-all.log | grep Test >&3
 rm -f CMakeCache.txt
 cmake .. -DCMAKE_C_COMPILER=riscv64-unknown-elf-gcc -DCMAKE_C_COMPILER_FORCED=ON -DALG_LIST="" -DIMPL_LIST=""
-cmake .. -DEMULATOR="qemu-system-riscv32;-semihosting;-nographic;-cpu;rv32;-bios;none;-kernel" \
+cmake .. -DCMAKE_BUILD_TYPE=$TYPE -UALG_LIST \
+         -DEMULATOR="qemu-system-riscv32;-semihosting;-nographic;-cpu;rv32;-bios;none;-kernel" \
          -DREL_FLAGS="$OPT;-fomit-frame-pointer;--specs=picolibc.specs;--oslib=semihost;-T../tests/rv32.ld;-march=rv32i;-mabi=ilp32" \
          -DDBG_FLAGS="$OPT;-std=c99;-Wall;-Wextra;-Wshadow;--specs=picolibc.specs;--oslib=semihost;-T../tests/rv32.ld;-march=rv32i;-mabi=ilp32" \
-         -DCMAKE_BUILD_TYPE=$TYPE -UALG_LIST \
-         -DIMPL_LIST="asm_rv32i"
+         -DIMPL_LIST="asm_rv32i;ref"
 cmake --build . --clean-first -- -k | tee -a ../test-all.log | grep "Built target genkat" >&3
 ctest | sed -u 's/[0-9.]* sec//g' | tee -a ../test-all.log | grep Test >&3
 
@@ -111,8 +111,8 @@ echo
 echo "Test mipsel $OPT builds:" | tee -a ../test-all.log | grep Test >&3
 rm -f CMakeCache.txt
 cmake .. -DCMAKE_C_COMPILER=mipsel-linux-gnu-gcc -DALG_LIST="" -DIMPL_LIST=""
-cmake .. -UALG_LIST -DEMULATOR="qemu-mipsel;-L;/usr/mipsel-linux-gnu" \
-         -DCMAKE_BUILD_TYPE=$TYPE \
+cmake .. -DCMAKE_BUILD_TYPE=$TYPE -UALG_LIST \
+         -DEMULATOR="qemu-mipsel;-L;/usr/mipsel-linux-gnu" \
          -DREL_FLAGS="$OPT;-fomit-frame-pointer" \
          -DDBG_FLAGS="$OPT;-std=c99;-Wall;-Wextra;-Wshadow" \
          -DIMPL_LIST=$CLIST
@@ -123,8 +123,8 @@ echo
 echo "Test mips $OPT builds:" | tee -a ../test-all.log | grep Test >&3
 rm -f CMakeCache.txt
 cmake .. -DCMAKE_C_COMPILER=mips-linux-gnu-gcc -DALG_LIST="" -DIMPL_LIST=""
-cmake .. -UALG_LIST -DEMULATOR="qemu-mips;-L;/usr/mips-linux-gnu" \
-         -DCMAKE_BUILD_TYPE=$TYPE \
+cmake .. -DCMAKE_BUILD_TYPE=$TYPE -UALG_LIST  \
+         -DEMULATOR="qemu-mips;-L;/usr/mips-linux-gnu" \
          -DREL_FLAGS="$OPT;-fomit-frame-pointer" \
          -DDBG_FLAGS="$OPT;-std=c99;-Wall;-Wextra;-Wshadow" \
          -DIMPL_LIST="esp32;opt32;opt32_lowsize;opt64;opt64_lowsize;ref"
