cea45c2590f9fbe08dbde587a7b852bb78d354ba
Martin Schläffer <martin.schlaeffer@gmail.com>
2024-10-18 15:38:58 +0200

    Simplify setting initial value in hash.c

--- crypto_hash/asconxofv12/avr/hash.c
+++ crypto_hash/asconxofv12/avr/hash.c
@@ -11,37 +11,24 @@
 
 #ifdef ASCON_HASH_BYTES
 
+#if ASCON_HASH_BYTES == 32 && ASCON_HASH_ROUNDS == 12
+#define IV(i) ASCON_HASH_IV##i
+#elif ASCON_HASH_BYTES == 32 && ASCON_HASH_ROUNDS == 8
+#define IV(i) ASCON_HASHA_IV##i
+#elif ASCON_HASH_BYTES == 0 && ASCON_HASH_ROUNDS == 12
+#define IV(i) ASCON_XOF_IV##i
+#elif ASCON_HASH_BYTES == 0 && ASCON_HASH_ROUNDS == 8
+#define IV(i) ASCON_XOFA_IV##i
+#endif
+
 forceinline void ascon_inithash(ascon_state_t* s) {
-  int i;
   /* initialize */
 #ifdef ASCON_PRINT_STATE
-#if ASCON_HASH_BYTES == 32 && ASCON_HASH_ROUNDS == 12
-  s->x[0] = ASCON_HASH_IV;
-#elif ASCON_HASH_BYTES == 32 && ASCON_HASH_ROUNDS == 8
-  s->x[0] = ASCON_HASHA_IV;
-#elif ASCON_HASH_BYTES == 0 && ASCON_HASH_ROUNDS == 12
-  s->x[0] = ASCON_XOF_IV;
-#elif ASCON_HASH_BYTES == 0 && ASCON_HASH_ROUNDS == 8
-  s->x[0] = ASCON_XOFA_IV;
-#endif
-  for (i = 1; i < 5; ++i) s->x[i] = 0;
+  *s = (ascon_state_t){{IV(), 0, 0, 0, 0}};
   printstate("initial value", s);
   P(s, 12);
 #else
-#if ASCON_HASH_BYTES == 32 && ASCON_HASH_ROUNDS == 12
-  const uint64_t iv[5] = {ASCON_HASH_IV0, ASCON_HASH_IV1, ASCON_HASH_IV2,
-                          ASCON_HASH_IV3, ASCON_HASH_IV4};
-#elif ASCON_HASH_BYTES == 32 && ASCON_HASH_ROUNDS == 8
-  const uint64_t iv[5] = {ASCON_HASHA_IV0, ASCON_HASHA_IV1, ASCON_HASHA_IV2,
-                          ASCON_HASHA_IV3, ASCON_HASHA_IV4};
-#elif ASCON_HASH_BYTES == 0 && ASCON_HASH_ROUNDS == 12
-  const uint64_t iv[5] = {ASCON_XOF_IV0, ASCON_XOF_IV1, ASCON_XOF_IV2,
-                          ASCON_XOF_IV3, ASCON_XOF_IV4};
-#elif ASCON_HASH_BYTES == 0 && ASCON_HASH_ROUNDS == 8
-  const uint64_t iv[5] = {ASCON_XOFA_IV0, ASCON_XOFA_IV1, ASCON_XOFA_IV2,
-                          ASCON_XOFA_IV3, ASCON_XOFA_IV4};
-#endif
-  for (i = 0; i < 5; ++i) s->x[i] = iv[i];
+  *s = (ascon_state_t){{IV(0), IV(1), IV(2), IV(3), IV(4)}};
 #endif
   printstate("initialization", s);
 }
